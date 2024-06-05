31a00c752b53ee611ed441c40fa8efdfd45b05b9
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-06-05 14:09:44 +0200

    Ongoing.

--- BokLoftet.Test/BokLoftetTests.cs
+++ BokLoftet.Test/BokLoftetTests.cs
@@ -2,20 +2,19 @@ using BokLoftet.Data;
 using BokLoftet.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace BokLoftet.Test
 {
-    public class BokLoftetTests : IDisposable
+    public class BokLoftetTests : IAsyncLifetime
     {
         private ApplicationDbContext _context;
-        private UserManager<ApplicationUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public BokLoftetTests()
+        public BokLoftetTests(UserManager<ApplicationUser> userManager)
         {
             _context = CreateDbContext();
-            _context.Database.EnsureCreated();
-            //InitializeAsync().Wait();
-            
+            _userManager = userManager;
         }
 
         private ApplicationDbContext CreateDbContext()
@@ -30,44 +29,51 @@ namespace BokLoftet.Test
         [Fact]
         public void DB_CheckIfCategoryDeckareExists()
         {
-            //var category = _context.Categories.FirstOrDefault(x => x.Name == "Deckare");
+            var category = _context.Categories.FirstOrDefault(x => x.Name == "Barnböcker");
 
-            //Assert.NotNull(category);
+            Assert.NotNull(category);
 
             // Arrage
-            _context.Categories.Add(new Category { Name = "Test" });
-            _context.SaveChanges();
+            //_context.Categories.Add(new Category { Name = "Test" });
+            //_context.SaveChanges();
 
 
 
-            // Act
-            var test = _context.Database.CanConnect();
-            var test1 = _context.Categories.FirstOrDefault(x => x.Name == "Test");
+            //// Act
+            //var test = _context.Database.CanConnect();
+            //var test1 = _context.Categories.FirstOrDefault(x => x.Name == "Test");
 
-            Assert.True(test);
-            Assert.NotNull(test1);
+            //Assert.True(test);
+            //Assert.NotNull(test1);
         }
 
-        private void SeedData()
+        private async Task SeedData()
         {
             // Categories
             List<Category> categories = [];
 
-            categories.Add(new Category { Name = "Deckare" });
-            categories.Add(new Category { Name = "Fantasy" });
-            categories.Add(new Category { Name = "Barnböcker" });
-            categories.Add(new Category { Name = "Skönlitteratur" });
-            categories.Add(new Category { Name = "Thriller" });
+            //categories.Add(new Category { Name = "Deckare" });
+            //categories.Add(new Category { Name = "Fantasy" });
+            //categories.Add(new Category { Name = "Barnböcker" });
+            //categories.Add(new Category { Name = "Skönlitteratur" });
+            //categories.Add(new Category { Name = "Thriller" });
+
+            var childrensBooks = new Category { Name = "Barnböcker" };
+            var thriller = new Category { Name = "Thriller" };
+
+            categories.Add(thriller);
+            categories.Add(childrensBooks);
 
             _context.Categories.AddRange(categories);
-
+            
             // Books
             List<Book> books = [];
 
             books.Add(new Book
             {
                 Author = "Astrid Lindgren",
-                Category = _context.Categories.First(x => x.Name == "Barnböcker"),
+                /*Category = _context.Categories.First(x => x.Name == "Barnböcker")*/ // Fungerar ej
+                Category = childrensBooks,
                 Title = "Pippi Långstrump",
                 Description = "En festlig bok om en stark liten flicka.",
                 Language = "Svenska",
@@ -75,12 +81,14 @@ namespace BokLoftet.Test
                 PublishYear = 1948,
                 Pages = 60,
                 ISBN = "9789129697285",
+                CoverImageURL = ""
             });
 
             books.Add(new Book
             {
                 Author = "Lee Child",
-                Category = _context.Categories.First(x => x.Name == "Thriller"),
+                /*Category = _context.Categories.First(x => x.Name == "Thriller")*/ // Fungerar ej
+                Category = thriller,
                 Title = "Jack Reacher",
                 Description = "En spännande bok en föredetta militärpolis.",
                 Language = "Engelska",
@@ -88,55 +96,67 @@ namespace BokLoftet.Test
                 PublishYear = 1997,
                 Pages = 592,
                 ISBN = "0-515-12344-7",
+                CoverImageURL = ""
             });
 
             _context.Books.AddRange(books);
 
             //User
 
-           //ApplicationUser user1 = new()
-           //{
-           //    FirstName = "Janne",
-           //    LastName = "Karlsson",
-           //    Adress = "Blomvägen 1, Göteborg",
-           //    Email = "janneloffe@karlsson.se",
-           //    NormalizedEmail = "JANNELOFFE@KARLSSON.SE",
-           //    PhoneNumber = "555 123 456",
-           //    UserName = "janneloffe@karlsson.se"
+            ApplicationUser user1 = new()
+            {
+                FirstName = "Janne",
+                LastName = "Karlsson",
+                Adress = "Blomvägen 1, Göteborg",
+                Email = "janneloffe@karlsson.se",
+                NormalizedEmail = "JANNELOFFE@KARLSSON.SE",
+                PhoneNumber = "555 123 456",
+                UserName = "janneloffe@karlsson.se"
 
-           //};
+            };
 
-           // await _userManager.CreateAsync(user1, "Test123!");
-           // await _userManager.AddToRoleAsync(user1, "Customer");
+            await _userManager.CreateAsync(user1, "Test123!");
+            await _userManager.AddToRoleAsync(user1, "Customer");
 
-           // ApplicationUser user2 = new()
-           // {
-           //     FirstName = "Greta",
-           //     LastName = "Svensson",
-           //     Adress = "Ringvägen 1, Göteborg",
-           //     Email = "greta@bokloftet.se",
-           //     NormalizedEmail = "GRETA@BOKLOFTET.SE",
-           //     PhoneNumber = "555 123 457",
-           //     UserName = "greta@bokloftet.se"
+            ApplicationUser user2 = new()
+            {
+                FirstName = "Greta",
+                LastName = "Svensson",
+                Adress = "Ringvägen 1, Göteborg",
+                Email = "greta@bokloftet.se",
+                NormalizedEmail = "GRETA@BOKLOFTET.SE",
+                PhoneNumber = "555 123 457",
+                UserName = "greta@bokloftet.se"
 
-           // };
+            };
 
 
-           // await _userManager.CreateAsync(user2, "Test123!");
-           // await _userManager.AddToRoleAsync(user2, "Admin");
+            await _userManager.CreateAsync(user2, "Test123!");
+            await _userManager.AddToRoleAsync(user2, "Admin");
 
             _context.SaveChanges();
         }
 
-        private async Task InitializeAsync()
+        //private async Task Initial()
+        //{
+        //    await _context.Database.EnsureCreatedAsync();
+        //    SeedData();
+        //}
+
+        //public void Dispose()
+        //{
+        //    _context.Database.EnsureDeleted();
+        //}
+
+        public async Task InitializeAsync()
         {
             await _context.Database.EnsureCreatedAsync();
-            SeedData();
+            await SeedData();
         }
 
-        public void Dispose()
+        public async Task DisposeAsync()
         {
-            _context.Database.EnsureDeleted();
+            await _context.Database.EnsureDeletedAsync();
         }
     }
 }
