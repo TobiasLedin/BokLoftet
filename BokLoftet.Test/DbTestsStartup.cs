899c3b2c65dd489a7a035ebf3406cb5ca922d796
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-06-05 16:03:32 +0200

    Test2

---
+++ BokLoftet.Test/DbTestsStartup.cs
@@ -0,0 +1,28 @@
+﻿using BokLoftet.Data;
+using BokLoftet.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BokLoftet.Test
+{
+    public class DbTestStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configurations, if needed.
+        }
+    }
+
+}
