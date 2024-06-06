33ef3e93089212dedfb6686509f51ccf647755ee
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-06-06 09:37:11 +0200

    Fixed register-function.

--- BokLoftet/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BokLoftet/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,16 @@ namespace BokLoftet.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            [Display(Name = "Firstname")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            [Display(Name = "Lastname")]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -79,6 +89,16 @@ namespace BokLoftet.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
+            [Display(Name = "Adress")]
+            public string Adress { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
+            [Display(Name = "Phone")]
+            public string Phone { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -113,11 +133,23 @@ namespace BokLoftet.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = CreateUser();
+                //var user = CreateUser();
+                
+                // Added /TL
+                var user = new ApplicationUser
+                { FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    Email = Input.Email,
+                    Adress = Input.Adress,
+                    PhoneNumber = Input.Phone,
+                    NormalizedUserName = Input.Email.ToUpper(),
+                    NormalizedEmail = Input.Email.ToUpper() 
+                };
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
+                await _userManager.AddToRoleAsync(user, "Customer");            // Added /TL
 
                 if (result.Succeeded)
                 {
